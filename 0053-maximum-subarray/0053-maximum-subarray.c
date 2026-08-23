9c47f1225003044fae19b941f4adf14fa2528cb2
Sagnik <sagnik8686@gmail.com>
2026-08-23 22:53:19 +0530

    Time: 1 ms (26.42%), Space: 15.5 MB (14.44%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.c
@@ -0,0 +1,11 @@
+int maxSubArray(int* nums, int numsSize) {
+    int max=-1000000,sum=0;
+    for(int i=0;i<numsSize;i++){
+        sum+=nums[i];
+        if(sum>max)
+            max=sum;
+        if(sum<0)
+            sum=0;
+    }
+    return max;
+}
