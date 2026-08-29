9533c927f098455c9f217465cdc404033d0b4f2f
Sagnik <sagnik8686@gmail.com>
2026-08-29 21:31:44 +0530

    Time: 2 ms (91.89%), Space: 47.6 MB (88.35%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i,a=0;
+        // for(i=0;i<nums.length;i++){
+        //     if(nums[i]==0)
+        //         count++;
+        // }
+        for(i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[a]=nums[i];
+                a++;
+            }
+        }
+        for(i=a;i<nums.length;i++){
+            nums[i]=0;
+        }
+    }
+}
